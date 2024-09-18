8aec7abd667370a0e53d539fa96d7c2f3639923b
Aman <bhaskarshah4235@gmail.com>
2024-09-18 23:21:35 +0530

    Hello world program

---
+++ Cshharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cshharp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
